0259793529e6ba76b9badc763db9158360af1b44
Aryan Mosavian Pour <aryanmp16@gmail.com>
2025-01-23 21:32:31 -0800

    prototyped parser function, restructured inst struct

--- include/assembler.h
+++ include/assembler.h
@@ -11,12 +11,12 @@ typedef struct inst {
   int m_opcode;
   char* m_name;
   int m_num_operands;
-  char** operands;
+  int operands[2]; //maximum number of operands is 2
 } inst;
 
 typedef struct expression {
   inst instruction;
-  int* operands; //registers represented as ENUMs, mem addresses are already integers
+  int* exp_operands; //registers represented as ENUMs, mem addresses are already integers
   //to prevent register ENUM from clashing with memory, mem addresses will start at
   //0x1000
 } expression;
@@ -33,5 +33,7 @@ extern int num_tokens;
 void create_instructions();
 void readfile(char* filename);
 token* lexer(); //must be called AFTER readfile
+void parser(token* token_stream);
+char* interpret_type(int type);
 
 #endif
