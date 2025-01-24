1d1e7bebcabda4d5676b2ff0e5ed01a13f5e628a
Aryan Mosavian Pour <aryanmp16@gmail.com>
2025-01-24 14:46:00 -0800

    first completely working parser!

--- src/assembler.c
+++ src/assembler.c
@@ -14,6 +14,7 @@ char* asp_file_contents = NULL;
 void missing_start();
 int is_delimeter(char i);
 int get_type(char* str);
+int encode_operand(token operand);
 
 void create_instructions() {
   instructions = (inst*)malloc(sizeof(inst) * NUM_INSTRUCTIONS);
@@ -21,7 +22,7 @@ void create_instructions() {
   inst LOI = {0x01, "LOI", 2, {IMMEDIATE, REGISTER}};
   inst ADD = {0x02, "ADD", 2, {REGISTER, REGISTER}};
   inst SUB = {0x03, "SUB", 2, {REGISTER, REGISTER}};
-  inst TER = {0xFF, "TER", 0, {-1, -1}};
+  inst TER = {0x04, "TER", 0, {-1, -1}};
 
   instructions[0] = LOI;
   instructions[1] = ADD;
@@ -146,16 +147,33 @@ inst token_to_inst(token t) {
 void parser(token* token_stream, int debug_mode) {
   int stream_traverser = 0;
   int num_expressions = 0;
+
+  FILE* f;
+  f = fopen("assembled", "w");
+  
   while (stream_traverser < num_tokens) {
     if (token_stream[stream_traverser].type == INSTRUCTION) {
       inst this_instruction = token_to_inst(token_stream[stream_traverser]);
-      debug_mode ? printf("Instruction: %s\tNumber of operands: %d", this_instruction.m_name, this_instruction.m_num_operands) : (void) 0;
+      debug_mode ? printf("Instruction: %s || Number of operands: %d", this_instruction.m_name, this_instruction.m_num_operands) : (void) 0;
+
+      debug_mode ? printf(" || Opcode: %d", this_instruction.m_opcode) : (void) 0;
+      fprintf(f, "0x%02x ", this_instruction.m_opcode);
+      
       if (this_instruction.m_num_operands != 0) {
-	debug_mode ? printf("\tOperands: ") : (void) 0;
+	debug_mode ? printf("\n\tOperands: ") : (void) 0;
 	for (int i = 0; i < this_instruction.m_num_operands; i++) {
 	  debug_mode ? printf("%s ", interpret_type(this_instruction.operands[i])) : (void) 0;
 	  if (token_stream[stream_traverser + i + 1].type == this_instruction.operands[i]) {
+	    //this is the branch we always want to be in.
+	    //This is the "things are going right" branch.
+	    //It is entered for every (correct) operand of an instruction.
 	    debug_mode ? printf("Y ") : (void) 0;
+	    debug_mode ? printf("Encoding: %d || ", encode_operand(token_stream[stream_traverser + i + 1])) : (void) 0;
+
+	    //operands will always be 16 bits (2 bytes), and we will store them in LITTLE endian. So for example, $0x4 as an input will be encoded as 0x0104, and will be stored as 0x04 0x01
+	    
+	    fprintf(f, "0x%02x ", encode_operand(token_stream[stream_traverser + i + 1]) & 0xFF); //lower byte of operand
+	    fprintf(f, "0x%02x ", (encode_operand(token_stream[stream_traverser + i + 1]) >> 8) & 0xFF); //upper byte of operand
 	  }
 	  else {
 	    debug_mode ? printf("X ") : (void) 0;
@@ -175,8 +193,10 @@ void parser(token* token_stream, int debug_mode) {
       printf("### Error: incorrect operand count at line %d ###\n", num_expressions + 1);
       exit(1);
     }
+    fprintf(f, "\n");
   }
   debug_mode ? printf("Total expressions read: %d\n", num_expressions) : (void) 0;
+  fclose(f);
 }
 
 char* interpret_type(int type) {
@@ -193,6 +213,60 @@ char* interpret_type(int type) {
     break;
   case INTEGER_LITERAL:
     strcpy(to_return, "INTEGER LITERAL");
+    break;
+  }
+  return to_return;
+}
+
+int encode_operand(token operand) {
+  int to_return = 0;
+  switch (operand.type) {
+  case REGISTER:
+    char str[3];
+    strncpy(str, operand.str + 1, 2);
+    if (!strcmp(str, "ra"))
+      to_return = rax;
+    else if (!strcmp(str, "rs"))
+      to_return = rsp;
+    else if (!strcmp(str, "ri"))
+      to_return = rip;
+    else if (!strcmp(str, "r1"))
+      to_return = r1;
+    else if (!strcmp(str, "r2"))
+      to_return = r2;
+    else if (!strcmp(str, "r3"))
+      to_return = r3;
+    else if (!strcmp(str, "r4"))
+      to_return = r4;
+    else if (!strcmp(str, "r5"))
+      to_return = r5;
+    else if (!strcmp(str, "r6"))
+      to_return = r6;
+    else if (!strcmp(str, "r7"))
+      to_return = r7;
+    else if (!strcmp(str, "r8"))
+      to_return = r8;
+    else if (!strcmp(str, "r9"))
+      to_return = r9;
+    else if (!strcmp(str, "r10"))
+      to_return = r10;
+    else if (!strcmp(str, "r11"))
+      to_return = r11;
+    else if (!strcmp(str, "r12"))
+      to_return = r12;
+    else {
+      printf("\nIncorrect register: %s\n", operand.str);
+      exit(1);
+    }
+    to_return += 0x0010; //this is where I decided registers are going to start from.
+    break;
+  case IMMEDIATE:
+    char* imm = operand.str + 3;
+    to_return = atoi(imm) + 0x0100; //start immediates from 0x0100. We will have at most 0x000F instructions, and we have 15 registers which take up to 0x001E, and 0x001F will be left empty.
+    break;
+  case INTEGER_LITERAL:
+    //no clue how I'm going to deal with this.
+    //MAKE SURE integer literals are AT MOST 16 bits (4 hex numbers)
     break;
   }
   return to_return;
